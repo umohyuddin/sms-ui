eb85e270faba4519489691682705ff05b8068a3b
uzair <uzair.anwar@expertflow.com>
2025-12-07 01:29:26 +0500

    fix fee catalog component into route

--- src/app/features/fee-catalog-component-management/pages/fee-catalog-component-details/fee-catalog-component-details.ts
+++ src/app/features/fee-catalog-component-management/pages/fee-catalog-component-details/fee-catalog-component-details.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
-import { AppConfigService } from '../../../../core/services/app-config.service';
-import { StandardResponse } from '../../../standard-management/models/standardResponse';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FeeCatalogComponentInfoComponent } from '../../components/fee-catalog-component-info/fee-catalog-component-info.component';
-import { FeeCatalogComponentManagementService } from '../../services/fee-catalog-component-management.service';
+import { ROUTES } from '../../../../core/const/APP_ROUTES';
 
 
 
@@ -17,34 +14,25 @@ import { FeeCatalogComponentManagementService } from '../../services/fee-catalog
   standalone: true,
 })
 export class FeeCatalogComponentDetails {
-  sectionData?: StandardResponse;
-  feeCatalogComponentId!: string;
-  constructor(private feeCatalogComponentManagementService: FeeCatalogComponentManagementService
-    , private route: ActivatedRoute,
-    private appConfig: AppConfigService
+  routedId: number | null = null;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router
   ) { }
 
-  ngOnInit(): void {
-    this.feeCatalogComponentId = this.route.snapshot.paramMap.get('id') ?? '';
-    console.log('Fee Catalog Component ID from route:', this.feeCatalogComponentId);
-    this.getSectionDetails(this.feeCatalogComponentId);
-  }
-
-  getSectionDetails(sectionId: string): void {
-    this.feeCatalogComponentManagementService.getFeeCatalogComponentsById(sectionId).subscribe({
-      next: (response: HttpResponse<any>) => {
-        console.log('✅ Status:', response.status);
-        console.log('📦 Body:', response.body);
-        this.sectionData = response.body;
-        console.log('standard Details:', this.sectionData);
-      },
-      error: (error) => {
-        console.error('❌ Error Status:', error.status);
-        console.error('Message:', error.message);
-      },
-      complete: () => {
-      }
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      this.routedId = id ? +id : null; // convert string to number
+      console.log('Resource ID from URL:', this.routedId);
     });
+  }
+  goToUpdatePage() {
+    if (this.routedId) {
+      this.router.navigate(ROUTES.FEE.FEE_CATALOG_COMPONENT.EDIT(this.routedId.toString()));
+    } else {
+      console.log('Resource ID from URL Not Found:');
+    }
   }
 }
 
