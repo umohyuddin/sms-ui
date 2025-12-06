85652485e70285edc866c79a43ae44f60f16d3e5
uzair <uzair.anwar@expertflow.com>
2025-12-07 01:46:03 +0500

    fix fee catalog component display issue

--- src/app/features/fee-catalog-component-management/components/fee-catalog-component-info/fee-catalog-component-info.component.ts
+++ src/app/features/fee-catalog-component-management/components/fee-catalog-component-info/fee-catalog-component-info.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { FeeCatalogComponentManagementService } from '../../services/fee-catalog-component-management.service';
 import { FeeCatalogComponentResponse } from '../../models/FeeCatalogComponentResponse';
+import { SmsUtil } from '../../../../core/utils/smsUtil';
 
 
 @Component({
@@ -14,35 +15,38 @@ import { FeeCatalogComponentResponse } from '../../models/FeeCatalogComponentRes
   styleUrls: ['./fee-catalog-component-info.component.css']
 })
 export class FeeCatalogComponentInfoComponent {
-  sectionData?: FeeCatalogComponentResponse;
-  sectionId!: string;
-  URL = '';
+  resourceData?: FeeCatalogComponentResponse;
+  routedId!: string;
   constructor(
     private feeCatalogComponentManagementService: FeeCatalogComponentManagementService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.sectionId = this.route.snapshot.paramMap.get('id') ?? '';
-    console.log('Section ID from route:', this.sectionId);
-    //this.getSectionDetails(this.sectionId);
+    this.routedId = this.route.snapshot.paramMap.get('id') ?? '';
+    console.log('ID from route:', this.routedId);
+    this.getFeeCatalogComponentDetails(this.routedId);
   }
 
-  // getSectionDetails(sectionId: string): void {
-  //   this.sectionManagementService.getSectionById(sectionId).subscribe({
-  //     next: (response) => {
-  //       console.log('✅ Success Status:', response.status);
-  //       console.log('📦 Response Body:', response.body);
-  //       this.sectionData = response.body;
-  //       console.log('📦 Standard data :', this.sectionData);
-  //     },
-  //     error: (error) => {
-  //       console.error('❌ Request Error Status:', error.status);
-  //       console.error('Message:', error.message);
-  //     },
-  //     complete: () => {
-  //       console.log('🔚 Request Complete');
-  //     }
-  //   })
-  // }
+  getFeeCatalogComponentDetails(routedId: string): void {
+    this.feeCatalogComponentManagementService.getFeeCatalogComponentsById(routedId).subscribe({
+      next: (response) => {
+        console.log('✅ Success Status:', response.status);
+        console.log('📦 Response Body:', response.body);
+        this.resourceData = response.body;
+        console.log('📦 Standard data :', this.resourceData);
+      },
+      error: (error) => {
+        console.error('❌ Request Error Status:', error.status);
+        console.error('Message:', error.message);
+      },
+      complete: () => {
+        console.log('🔚 Request Complete');
+      }
+    })
+  }
+
+  getInitials(name?: string): string {
+    return SmsUtil.getInitials(name ?? '');
+  }
 }
