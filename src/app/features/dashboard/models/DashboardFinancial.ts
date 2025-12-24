876010ddd7389ad51607a16ffd152113249ea56b
uzair <uzair.anwar@expertflow.com>
2025-12-24 15:00:57 +0500

    working on dashboard financials

--- src/app/features/dashboard/models/DashboardFinancial.ts
+++ src/app/features/dashboard/models/DashboardFinancial.ts
@@ -4,10 +4,12 @@ export interface DashboardFinancial {
   collectedMonthlyFee: number;
   outstandingMonthlyFee: number;
   collectionPercentageMonthlyFee: number;
+  monthName:string
 
   // Yearly stats
   assignedYearlyFee: number;
   collectedYearlyFee: number;
   outstandingYearlyFee: number;
   collectionPercentageYearlyFee: number;
+  academicYearName:string;
 }
