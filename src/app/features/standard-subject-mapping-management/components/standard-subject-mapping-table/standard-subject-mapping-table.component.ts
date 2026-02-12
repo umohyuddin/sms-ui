b48e6556f25580bf5bc141fea72d2cc0e1e00ac4
uzair <uzair.anwar@expertflow.com>
2026-02-13 00:10:47 +0500

    subject group progress bar

--- src/app/features/standard-subject-mapping-management/components/standard-subject-mapping-table/standard-subject-mapping-table.component.ts
+++ src/app/features/standard-subject-mapping-management/components/standard-subject-mapping-table/standard-subject-mapping-table.component.ts
@@ -38,6 +38,26 @@ export class StandardSubjectMappingTableComponent implements OnInit, OnDestroy {
         return this.assignedSubjects.filter(a => !a.isMandatory).length;
     }
 
+    get availablePercent(): number {
+        if (!this.allSubjects.length) return 0;
+        return (this.availableSubjects.length / this.allSubjects.length) * 100;
+    }
+
+    get assignedPercent(): number {
+        if (!this.allSubjects.length) return 0;
+        return (this.assignedSubjects.length / this.allSubjects.length) * 100;
+    }
+
+    get mandatoryPercent(): number {
+        if (!this.assignedSubjects.length) return 0;
+        return (this.mandatoryCount / this.assignedSubjects.length) * 100;
+    }
+
+    get electivePercent(): number {
+        if (!this.assignedSubjects.length) return 0;
+        return (this.electiveCount / this.assignedSubjects.length) * 100;
+    }
+
     private destroy$ = new RxSubject<void>();
 
     constructor(private mappingService: StandardSubjectMappingService) { }
